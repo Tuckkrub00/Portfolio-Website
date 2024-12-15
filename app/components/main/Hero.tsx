54ef4b1f20f3c22518212ddb399c6b951da8bfa8
Tuckkrub00 <145004375+Tuckkrub00@users.noreply.github.com>
2024-12-15 11:54:57 +0700

    first version

---
+++ app/components/main/Hero.tsx
@@ -0,0 +1,32 @@
+import { HeroContent } from "../sub/HeroContent";
+
+const Hero = () => {
+  return (
+    <div className=" relative flex flex-col h-full w-full">
+      {/* <video
+        autoPlay={true}
+        loop={true}
+        muted={true}
+        className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover"
+        // className=" absolute  left-0 z-[1] w-full h-full object-cover"
+      >
+        <source src="/webm/blackhole.webm" type="video/webm" />
+      </video> */}
+      <video
+        autoPlay={true}
+        loop={true}
+        muted={true}
+        // className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover"
+        className="transform scale-x-[-1] absolute  left-0 z-[1] w-full h-full object-cover"
+      >
+        <source
+          src="/webm/tendou-alice-blue-archive-1-moewalls-com.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <HeroContent></HeroContent>
+    </div>
+  );
+};
+
+export default Hero;
