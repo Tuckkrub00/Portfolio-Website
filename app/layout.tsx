1730607652f00a5a5682b44b97af11ff8ba5c53e
Tuckkrub00 <145004375+Tuckkrub00@users.noreply.github.com>
2024-12-16 17:04:49 +0700

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Footer } from "./components/main/Footer";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={` overflow-scroll overflow-x-hidden`}
       >
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
