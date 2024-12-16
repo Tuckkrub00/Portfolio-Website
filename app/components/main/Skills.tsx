0d1471244732c6b9d95fecfbe9615627c81587d8
Tuckkrub00 <145004375+Tuckkrub00@users.noreply.github.com>
2024-12-16 15:15:50 +0700

    project v1

--- app/components/main/Skills.tsx
+++ app/components/main/Skills.tsx
@@ -14,6 +14,7 @@ const Skills = () => {
     <section
       className="flex flex-col items-center  justify-center gap-3 h-full relative overflow-hidden py-20"
       // className="flex flex-col items-center  justify-center gap-3 h-full relative overflow-hidden"
+      id="skills"
       style={{ transform: "scale(0.9)" }}
     >
       <SkillText />
